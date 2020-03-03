7a58a7bb646e7f766f1a419e4b8322b0e36b5115
Simon Fedyukevich <fedukevichsay@gmail.com>
2020-03-03 10:34:52 +0700

    new

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -7,10 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Tobacco Stick = new Stick ("IQOS", 0.9, 20);
         Tobacco Cigarette = new Cigarette("Winston", 0.6, 20);
-        List<Tobacco> shop = Arrays.asList(Cigarette,Stick);
-        for (Tobacco d: shop){
-            
-        }
+
 
     }
 }
